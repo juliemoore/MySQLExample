fd1a2f3de02ff45b4e2c7021a14f5f2605df3a74
Julie Moore <msjulieannmoore@gmail.com>
2018-01-09 01:41:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/julieannmoore/mysqlexample/DbStrings.java
@@ -0,0 +1,12 @@
+package com.example.julieannmoore.mysqlexample;
+
+/**
+ * Created by Julie Ann Moore on 1/8/2018.
+ */
+
+public class DbStrings {
+    static final String DATABASE_URL = "10.0.2.2:3306";
+    static final String DATABASE_NAME = "mhs_example";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "hNUxQqMUgCQabti0*";
+}
